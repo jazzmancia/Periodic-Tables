5f059cc66a0a8f6501387248d732392a599d20c7
Jazz <jazzmancia@gmail.com>
2021-12-27 14:12:20 -0800

    remove duplicate cancel buttons

--- front-end/src/dashboard/Reservations.js
+++ front-end/src/dashboard/Reservations.js
@@ -29,8 +29,7 @@ function Reservations({onCancel, reservations = [] }) {
               <div className="col-sm-1">
                 <Link className="btn" to={`/reservations/${reservation.reservation_id}/seat`}>seat</Link>
                 <Link className="btn" to={`/reservations/${reservation.reservation_id}/edit`}>edit</Link>
-                <Link className="btn" to={`/reservations/${reservation.reservation_id}/cancel`}>cancel</Link>
-                <button type="button" className="btn cancel" data-reservation-id-cancel={reservation.reservation_id} onClick={cancelHandler}>Cancel</button>
+                <button type="button" className="btn cancel" data-reservation-id-cancel={reservation.reservation_id} onClick={cancelHandler}>cancel</button>
               </div>
           ) : ( "" )}
         </div>
